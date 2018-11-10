334acfa9e0b97be004ce08d410726f6558b8aea5
Stéphane <5939522+stephanyan@users.noreply.github.com>
2018-11-10 13:25:00 +0100

    Update test.c: change array names in test.c
    No need to number arrays because they are not in the same scope

--- test.c
+++ test.c
@@ -3,26 +3,26 @@
 #include "functions.c"
 
 Test(minFunctionTests, mininum_should_be_minus_110) {
-    int array1[7] = {-52, 56, 30, 29, -54, 0, -110};
-    cr_assert_eq(min(array1, 7), -110);
+    int array[7] = {-52, 56, 30, 29, -54, 0, -110};
+    cr_assert_eq(min(array, 7), -110);
 }
 
 Test(minFunctionTests, mininum_should_be_0) {
-    int array2[5] = {42, 54, 65, 87, 0};
-    cr_assert_eq(min(array2, 5), 0);
+    int array[5] = {42, 54, 65, 87, 0};
+    cr_assert_eq(min(array, 5), 0);
 }
 
 Test(maxFunctionTests, maximum_should_be_566) {
-    int array3[8] = {4,6,2,1,9,63,-134,566};
-    cr_assert_eq(max(array3, 8), 566);
+    int array[8] = {4,6,2,1,9,63,-134,566};
+    cr_assert_eq(max(array, 8), 566);
 }
 
 Test(maxFunctionTests, maximum_should_be_5) {
-    int array4[1] = {5};
-    cr_assert_eq(max(array4, 1), 5);
+    int array[1] = {5};
+    cr_assert_eq(max(array, 1), 5);
 }
 
 Test(maxFunctionTests, maximum_should_be_56) {
-    int array5[7] = {-52, 56, 30, 29, -54, 0, -110};
-    cr_assert_eq(max(array5, 7), 56);
+    int array[7] = {-52, 56, 30, 29, -54, 0, -110};
+    cr_assert_eq(max(array, 7), 56);
 }
